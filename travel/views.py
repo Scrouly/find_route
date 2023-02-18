80bb092fb5383cd72fd309657bf388e2e82e03fe
Scrouly <fevanwood@yahoo.com>
2023-02-18 18:13:11 +0300

    create Cities app

--- travel/views.py
+++ travel/views.py
@@ -6,3 +6,9 @@ def home(request):
     name = "Bob"
     context = {'name': name}
     return render(request, 'travel/home.html', context)
+
+
+def about(request):
+    name = "About us"
+    context = {'name': name}
+    return render(request, 'travel/about.html', context)
